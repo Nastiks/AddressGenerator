8a62f23497f1a927150896d3de99e2021280f8dd
Anastasiia Belova <93551303+Nastiks@users.noreply.github.com>
2022-12-03 00:15:38 +0600

    Change file path

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,14 @@
-﻿namespace AddressGenerator.Models
+using System.Diagnostics.Metrics;
+using System.IO;
+
+namespace AddressGenerator.Models
 {
     public class User
     {
         private static SimpleRandom? random;
 
         private string[] Gender = { "male", "female" };
+        private const string dataFolder = "data";
         
         public int Id { get; set; }
         public int Number { get; set; }
@@ -33,25 +37,26 @@
         public string GenerateName(string country)
         {
             string gender = random!.Next(2) == 0 ? Gender[0] : Gender[1];
-            string[] firstNames = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\{gender}_firstName.txt");
-            string[] lastNames = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\{gender}_lastName.txt");
+            string[] firstNames = File.ReadAllLines(Path.Combine(dataFolder, country, $"{gender}_firstName.txt"));
+            string[] lastNames = File.ReadAllLines(Path.Combine(dataFolder, country, $"{gender}_lastName.txt"));
             string fullName = $"{firstNames[random.Next(firstNames.Length)]} {lastNames[random.Next(lastNames.Length)]}";
             return fullName;
         }
 
         public string GenerateAddress(string country)
         {
-            string[] regions = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\region.txt");
-            string[] cities = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\city.txt");
-            string[] streets = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\street.txt");
-            string address = $"{country} {regions[random!.Next(regions.Length)]} {cities[random!.Next(cities.Length)]} {streets[random!.Next(streets.Length)]} {random.Next(150)}";
+            string[] regions = File.ReadAllLines(Path.Combine(dataFolder, country, "region.txt"));
+            string[] cities = File.ReadAllLines(Path.Combine(dataFolder, country, "city.txt"));
+            string[] streets = File.ReadAllLines(Path.Combine(dataFolder, country, "street.txt"));
+            string address = $"{country} {regions[random!.Next(regions.Length)]} {cities[random!.Next(cities.Length)]}" +
+                $" {streets[random!.Next(streets.Length)]} {random.Next(150)}";
             return address;
         }
 
         public string GeneratePhone(string country)
         {
-            string[] codeCountry = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\phone_codeCountry.txt");
-            string[] regionCodes = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\phone_regionCode.txt");
+            string[] codeCountry = File.ReadAllLines(Path.Combine(dataFolder, country, "phone_codeCountry.txt"));
+            string[] regionCodes = File.ReadAllLines(Path.Combine(dataFolder, country, "phone_regionCode.txt"));
             string phone = $"{codeCountry[0]} {regionCodes[random!.Next(regionCodes.Length)]} {random.Next(00000, 99999)}";
             return phone;
         }
@@ -78,27 +83,25 @@
 
         public static string DistributeErrors(string value, string country)
         {
-            switch (random!.Next(1, 3))
+            return random!.Next(1, 3) switch
             {
-                case 1:
-                    return DeletingSymbol(value);
-                    case 2:
-                    return AddingSymbol(value, country);
-                default:
-                    return RearrangeSymbol(value);
-            }
+                1 => DeletingSymbol(value),
+                2 => AddingSymbol(value, country),
+                _ => RearrangeSymbol(value),
+            };
         }
 
         public static string DeletingSymbol(string value) => value.Remove(random!.Next(value.Length), 1);
         
         public static string AddingSymbol(string value, string country)
         {
-            string[] letters = File.ReadAllLines($"C:\\sources\\Task5Work\\AddressGenerator\\wwwroot\\{country}\\letter.txt");
+            string[] letters = File.ReadAllLines(Path.Combine(dataFolder, country, "letter.txt"));
             string alphabet = letters[0];
-            string result = value.Insert(random!.Next(value.Length), (alphabet[random!.Next(random!.Next(value.Length))]).ToString());
+            string result = value.Insert(random!.Next(value.Length), 
+                alphabet[random!.Next(random!.Next(value.Length))].ToString());
             return result;
         }
 
-        public static string RearrangeSymbol(string value) => value[1] + value[0] + value.Substring(2);        
+        public static string RearrangeSymbol(string value) => value[1] + value[0] + value[2..];        
     }
 }
